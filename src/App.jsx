93b3b4450983c62b15b42d9bb00c75b348637b75
Joe-Itax <josephitakala18@gmail.com>
2024-01-30 14:36:24 +0000

    feat: add validation for all inputs

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ export default function App() {
     <main>
       <h1>Contact Form</h1>
       <div>
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(onSubmit)} noValidate>
           <div>
             <label>Nom</label>
             <input
@@ -36,6 +36,14 @@ export default function App() {
               name="nom"
               {...register("name", {
                 required: "Ce champ est obligatoire",
+                minLength: {
+                  value: 3,
+                  message: "Le nom doit faire au moins 3 caractères",
+                },
+                maxLength: {
+                  value: 25,
+                  message: "Le nom doit faire au plus 25 caractères",
+                },
               })}
             />
             {errors.name && (
@@ -61,19 +69,48 @@ export default function App() {
           </div>
           <div>
             <label>Email</label>
-            <input type="email" name="email" {...register("email")} />
+            <input
+              type="email"
+              name="email"
+              {...register("email", {
+                required: "Ce champ est obligatoire",
+                pattern: {
+                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                  message: "Ce champ n'est pas au bon format",
+                },
+              })}
+            />
+            {errors.email?.message && (
+              <span style={{ color: "red" }}>{errors.email?.message}</span>
+            )}
           </div>
           <div>
             <label>Age</label>
-            <input type="number" name="age" {...register("age")} />
+            <input
+              type="number"
+              name="age"
+              {...register("age", {
+                required: "Ce champ est obligatoire",
+              })}
+            />
+            {errors.age?.message && (
+              <span style={{ color: "red" }}>{errors.age?.message}</span>
+            )}
           </div>
           <div>
             <label>Genre</label>
-            <select {...register("gender")}>
+            <select
+              {...register("gender", {
+                required: "Ce champ est obligatoire",
+              })}
+            >
               <option value={"femme"}>Femme</option>
               <option value={"homme"}>Homme</option>
               <option value={"autre"}>Autre</option>
             </select>
+            {errors.gender?.message && (
+              <span style={{ color: "red" }}>{errors.gender?.message}</span>
+            )}
           </div>
           <button type="submit">Enregistrer</button>
         </form>
